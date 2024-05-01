365b9d5c7ded158c2924cf994c3f31fab19afd45
Sharique9199 <mdshariqueraj8292@gmail.com>
2024-05-01 12:05:09 +0530

    cardList

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,13 +1,14 @@
 
 import React from 'react'
 import Layout from '../../Layout'
+import ItemsList from '../../components/Card/ItemsList'
 // import Layout from '../../Layout'
 
 
 const Home = () => {
   return (
     <Layout>
-      
+     <ItemsList/>
     </Layout>
   )
 }
